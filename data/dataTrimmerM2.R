5b9d4be4b5d4a4f739963d0ac73bbbe4074d9ff2
Arif <ariful35-937@diu.edu.bd>
2023-10-24 18:22:06 +0200

    Cleaned and Reformated M2 Experimental data as RData files

---
+++ data/dataTrimmerM2.R
@@ -0,0 +1,43 @@
+# Install the jsonlite package (if not already installed)
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+  install.packages("jsonlite")
+}
+
+# Load the jsonlite library
+library(jsonlite)
+
+# Create a list of file paths to JSON files
+file_paths <- c(
+  "chatgpt/m2-generation-chatGPT.json",
+  "chatgpt/m2-knowledge-chatGPT.json",
+  "chatgpt/m2-realWorld-chatGPT.json",
+  "bard/m2-generation-bard.json",
+  "bard/m2-knowledge-bard.json",
+  "bard/m2-realWorld-bard.json",
+  "llama/m2-generation-llama2.json",
+  "llama/m2-knowledge-llama2.json",
+  "llama/m2-realworld-llama2.json"
+)
+
+# function to format and save the counter data as RData
+format_and_save_rdata <- function(file_path) {
+  # Load JSON data from the file
+  data <- fromJSON(file_path)
+  
+  # Access the 'counters' element
+  counter_data <- data[["counters"]]
+  
+  # Specify the output RData file path to save the counter data
+  output_rdata_file <- gsub(".json$", ".RData", file_path)
+  
+  # Save the counter data as RData
+  save(counter_data, file = output_rdata_file)
+  
+  # Print a message indicating the processing is done
+  cat("Formatted and saved counter data from", file_path, "as", output_rdata_file, "\n")
+}
+
+# Loop through the list of file paths and format/save counter data as RData
+for (file_path in file_paths) {
+  format_and_save_rdata(file_path)
+}
