36688396d5a74c287c9728fe810181bcb87728fa
Asror Akbarkhodjaev <asror9800@mail.ru>
2023-10-29 12:02:57 +0100

    Made some changes to scripts, and wrote down part of README

--- Normality.R
+++ Normality.R
@@ -4,6 +4,10 @@
 library(jsonlite)
 library(ggplot2) 
 
+machines <- c('m1','m2','windows11')
+llms <- c('chatgpt','bard','llama')
+tasks <- c('generation','knowledge','realworld')
+
 remove_outliers <- function(data) {
   data <- data[data != 0.00]
   q <- quantile(data)
@@ -14,29 +18,69 @@ remove_outliers <- function(data) {
   return(data_filtered)
 }
 
-m1_generation_chatgpt_data <- fromJSON("data/formatted-data/json/m1/m1-generation-chatgpt-formated.json")
-m1_generation_chatgpt_df <- as.data.frame(m1_generation_chatgpt_data) 
-
-##### m1 chatgpt generation power ######
-plot.ts(m1_generation_chatgpt_df$power.usage, main = "Power usage", 
+for (m in machines) {
+  for (t in tasks) {
+    for (l in llms) {
+      fname = paste0(m,'-',t,'-',l,'-formated')
+      json_path = paste0('data/formatted-data/json/',m,'/',fname,'.json') 
+      
+      task_data <- fromJSON(json_path)
+      task_df <- as.data.frame(task_data)
+      
+      ##### power usage ######
+      plot.ts(task_df$power.usage, main = paste(m,t,l,"power usage"), 
         xlab= "Timepoints", ylab = "Power (microwatt)")
+        
+      unlist_task_df_pwu <- unlist(task_df$power.usage, use.names=FALSE)
+      filtered_task_df_pwu <- remove_outliers(unlist_task_df_pwu)
+      hist(filtered_task_df_pwu, main = paste("Histogram for Power usage of",m,t,l))
+             
+      qqnorm(filtered_task_df_pwu, main = paste("QQ Plot for Power usage",m,t,l), 
+        xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
+        qqline(filtered_task_df_pwu)
+      
+      ##### cpu usage ######
+      plot.ts(task_df$cpu.usage, main = paste(m,t,l,"CPU usage"),
+              xlab= "Timepoints", ylab = "CPU usage (microwatt)")
+      
+      unlist_task_df_cpu <- unlist(task_df$cpu.usage, use.names=FALSE)
+      filtered_task_df_cpu <- remove_outliers(unlist_task_df_cpu)
+      hist(filtered_task_df_cpu, main = paste("Histogram for CPU usage of",m,t,l))
+      
+      qqnorm(filtered_task_df_cpu, main = paste("QQ Plot for CPU usage",m,t,l), 
+             xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
+      qqline(filtered_task_df_cpu)
+      
+      rm(task_data,task_df)
+      rm(unlist_task_df_pwu,filtered_task_df_pwu)
+      rm(unlist_task_df_cpu,filtered_task_df_cpu)
+    }
+  }
+}
 
-unlist_m1_pwu <- unlist(m1_generation_chatgpt_df$power.usage, use.names=FALSE)
-filtered_m1_pwu <- remove_outliers(unlist_m1_pwu)
-hist(filtered_m1_pwu, main = "Histogram for Power usage")
 
-qqnorm(filtered_m1_pwu, main = "QQ Plot for Power usage", 
-       xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
-qqline(filtered_m1_pwu)
-
-##### m1 chatgpt generation cpu ######
-plot.ts(m1_generation_chatgpt_df$cpu.usage, main = "CPU usage",
-        xlab= "Timepoints", ylab = "CPU usage (microwatt)")
-
-unlist_m1_cpu <- unlist(m1_generation_chatgpt_df$cpu.usage, use.names=FALSE)
-filtered_m1_cpu <- remove_outliers(unlist_m1_cpu)
-hist(filtered_m1_cpu, main = "Histogram for CPU usage")
-
-qqnorm(filtered_m1_cpu, main = "QQ Plot for CPU usage", 
-       xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
-qqline(filtered_m1_cpu)
+m = 'linux'
+  for (t in tasks) {
+    for (l in llms) {
+      fname = paste0(m,'-',t,'-',l,'-formated')
+      json_path = paste0('data/formatted-data/json/',m,'/',fname,'.json') 
+      
+      task_data <- fromJSON(json_path)
+      task_df <- as.data.frame(task_data)
+      
+      ##### cpu usage ######
+      plot.ts(task_df$cpu.usage, main = paste(m,t,l,"CPU usage"),
+              xlab= "Timepoints", ylab = "CPU usage (microwatt)")
+      
+      unlist_task_df_cpu <- unlist(task_df$cpu.usage, use.names=FALSE)
+      filtered_task_df_cpu <- remove_outliers(unlist_task_df_cpu)
+      hist(filtered_task_df_cpu, main = paste("Histogram for CPU usage of",m,t,l))
+      
+      qqnorm(filtered_task_df_cpu, main = paste("QQ Plot for CPU usage",m,t,l), 
+             xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
+      qqline(filtered_task_df_cpu)
+      
+      rm(task_data,task_df)
+      rm(unlist_task_df_cpu,filtered_task_df_cpu)
+    }
+  }
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Green-Code-Guardians
 
 This repository is for the code portion of Green Lab course from VU Amsterdam. The code portion includes the interaction script, the experiment configuration of experiment-runner, and the R data analysis.
+
+
+### Normality testing
+To check normality of our data you can run Normality.R script in the root directory. It will generate histograms, qq-plots and timeline distribution of value per Machine-Task-LLM. Moreover it will generate this visualisations for both cpu and power usage. 
+
+### Hypothesis testing
+The First Research Question Hypothesis is tested inside of Power-Kruskal-Wallis-test.R script in the root directory. When you run this script it will print the values for all the tests and their corresponding p-value. 
+
+### Power consumption in Watts
+If you want to check the power consumption of the experiment per machine per llm per task, you can run Power-usage-Watts.R script in the root directory.
